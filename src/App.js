f2a6200eda06b3402e2357f8f1fa4d1beec5d896
Dean Asadi <eng.de86@gmail.com>
2018-11-27 16:53:49 -0600

    working on components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 
+//Components
+import Auth from "./components/Auth/Auth";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Form from "./components/Form/Form";
+import Nav from "./components/Nav/Nav";
+import Post from "./components/Post/Post";
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +13,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header"> Testing React... </header>
+        <div>
+          <Auth />
+          <Dashboard />
+          <Form />
+          <Nav />
+          <Post />
+        </div>
       </div>
     );
   }
