11795a8e483ab7c81d4dfb5a737b45ed7985353b
Dean Asadi <eng.de86@gmail.com>
2018-11-27 16:22:21 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header"> Testing... </header>
+        <header className="App-header"> Testing React... </header>
       </div>
     );
   }
